a6cbc452df1d66bf5bd05181c6d78c9a2ed0ed9d
habibuga <vitaliy.ryzhynsky@gmail.com>
2022-08-08 22:02:45 +0200

    home app and config router

--- config/settings.py
+++ config/settings.py
@@ -42,7 +42,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
     'users.apps.UsersConfig',
+    'home.apps.HomeConfig',
 ]
 
 MIDDLEWARE = [
@@ -60,7 +62,9 @@ ROOT_URLCONF = 'config.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'templates'),
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- example.py
+++
@@ -1,2 +0,0 @@
-from django.core.management.utils import get_random_secret_key
-get_random_secret_key()
